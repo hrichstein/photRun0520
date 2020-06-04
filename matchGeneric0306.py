ee0d742efd253ab587bacc029bd34c425ab98a89
hcrichstein <hannah.richstein@live.com>
2020-06-04 14:51:11 -0400

    still working... matchGen doesn't really work

--- matchGeneric0306.py
+++ matchGeneric0306.py
@@ -11,10 +11,16 @@ def matchXYs(master,match,matchtol=1,outName='welp'):
 
     idx_mat = np.zeros((len(match), 1))
     idx_mat[:,0] = np.arange(0,len(match),1)
-    matchids = np.zeros((len(master), 1))
+
+    idx_mas = np.zeros((len(master), 1))
+    idx_mas[:,0] = np.arange(0,len(master),1)
+
+    mas_ids = np.zeros((len(master), 1))
 
     matcher = np.hstack((match[:,[0,1]],idx_mat))
-    matchCat = matcher.copy()
+    # matcher = np.hstack((match[:,[0,1]],idx_mat))
+
+    # matchCat = matcher.copy()
 
     nF = True
     row = 0
@@ -25,9 +31,11 @@ def matchXYs(master,match,matchtol=1,outName='welp'):
         # matchrows = match[ (abs(y_mas[row] - y_mat[row]) <= matchtol) ]
 
         if (len(matchrows) == 1):
-            matchids[row] = matcher[row][2]
-            row = row + 1
-            print('match')
+            mas_ids[row] = matcher[row][2]
+            row += 1
+            # matchids[row] = matcher[row][2]
+            # row = row + 1
+            # print('match')
 
         # elif (len(matchrows) > 1):
         #     distDiff = np.zeros((len(matchrows),1))
@@ -42,16 +50,18 @@ def matchXYs(master,match,matchtol=1,outName='welp'):
             print(row)
             # print(len(matcher))
             master = np.delete(master, row, 0)
-            # match = np.delete(match, row, 0)
-            matchids = np.delete(matchids,row,0)
+            mas_ids = np.delete(mas_ids,row,0)
+            matcher = np.delete(matcher,row,0)
+            match = np.delete(match, row, 0)
+            # matchids = np.delete(matchids,row,0)
             # matchCat = np.delete(matchCat,row,0)
-            # idx_mat = np.delete(idx_mat,row,0)
+            idx_mat = np.delete(idx_mat,row,0)
 
         if (row >= len(master)):
             nF = False
             print('Master Length:',len(master))
 
-    outArr = np.hstack((master,matchids))
+    outArr = np.hstack((master,mas_ids,matcher))
 
     np.savetxt(dir+outName+'.dat',outArr,fmt='%1.5f %1.5f %d', header='x y matchID')
 
@@ -60,4 +70,4 @@ def matchXYs(master,match,matchtol=1,outName='welp'):
 master = psf[:,[0,1]]
 match = np.genfromtxt(dir+'flc2PSF_round1.dat')
 
-matchXYs(master,match,matchtol=1,outName='flc2PSF_match_r1')
+matchXYs(master,match,matchtol=1,outName='flc2PSF_match_r1_0406')
